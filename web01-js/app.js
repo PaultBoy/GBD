1e8bbaf0bea98c0d87d0a21c8a7caa8710e8b75b
PabloVT01 <200096@iessierradeguara.com>
2020-12-03 16:35:55 +0100

    Primeros pasos JS

---
+++ web01-js/app.js
@@ -0,0 +1,20 @@
+function Prueba() {
+    document.getElementById("1").innerHTML = "Holamundo mundo"
+}
+//let: Variables locales. var: Variables globales
+let texto = "Esto es un \"texto\"";
+let numero = 25;
+let decimal = 5.4;
+let booleano = true;
+
+let array = [3, 5, 2];
+
+let persona = {
+    "nombre": "Manuel",
+    "edad": 23,
+    "cabello": "Rubio"
+}
+
+console.log(persona)
+console.log(persona["nombre"])
+console.log(array[1])
