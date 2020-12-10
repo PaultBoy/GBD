ebb4be83a2209a6e3d2aae6e5e31101938b13ff1
PabloVT01 <200096@iessierradeguara.com>
2020-12-10 17:41:11 +0100

    Ejercicio para casa

--- web02-js/app.js
+++ web02-js/app.js
@@ -8,3 +8,27 @@ for (const alumno of datos) {
     }
 }
 
+//Modificad el programa para que sean listas anidadas.
+//* Alumno (acg63):
+//  - tiempo: 1898
+//  ---
+//* Alumno (adf73):
+//  - tiempo: 180
+//  - ...
+//* Alumno...
+
+for (const alumno of datos) {
+    let alu = document.createElement("ul");
+    document.body.appendChild(alu);
+    let id = document.createElement("li");
+    id.innerHTML = alumno.identificadorAlumno;
+    alu.appendChild(id);
+    let param = document.createElement("ul");
+    id.appendChild(param);
+    for (const prop in alumno) {
+        if(prop != 0){
+            let el = document.createElement("li");
+            el.innerHTML = prop + ": " + alumno[prop];
+        }
+    }
+}
